12e358cad7cdce2720a01dfd7c9492bf89d80555
cuberJ <43082093+cuberJ@users.noreply.github.com>
2020-05-22 11:39:09 +0800

    做了点修改
    稍微改了点之前的类设定，然后完善了几个函数。run的部分，关于时钟同步没有构思好，感觉会出事，后面慢慢改吧

--- Basic.h
+++ Basic.h
@@ -5,6 +5,7 @@
 #include<vector>
 #include<algorithm>
 #include<thread>
+#include<mutex>
 using namespace std;
 
 /*
@@ -46,7 +47,21 @@ struct EquipInfo //外设信息
 
 class PCB //PCB相当于就是一个进程类
 {
-private:
+public:
+	string PID; //这里没有想好到底怎么分配PID，要不按照创建时间分配？
+	int priority = priority;
+	int WaitingTime;
+	int RunTime;
+	int status = 0;  //进程所处的状态
+
+
+	// 逻辑地址，记录下一条要执行的指令的虚拟地址
+	int PageNumber;  //虚拟页号
+	int PageIndex; //虚拟页内偏移量
+
+	// 设备，记录当前进程在申请的设备信息
+	vector<EquipInfo> Equipnum; // 记录申请的设备的编号
+
 	PCB(int priority);
 
 	~PCB(); // 析构函数，用于进程结束的时候释放进程
@@ -59,23 +74,27 @@ public:
 
 class CPUSimulate //模拟CPU运行
 {
-private:
-	bool isFinish; //标记当前执行的指令是否执行完毕
+public:
+	bool isFinish = true; //标记当前执行的指令是否执行完毕
+	bool Lock = false; // 运行锁，当interrupt执行的时候将其锁住，禁止下一个指令进入
+	mutex interptLock; //中断使用的锁
+	string order;  //IF段读取的指令，送入ID段分析
 	vector<PCB>READY;  //就绪队列
 	vector<PCB>BREAK;  // 中断队列
+	vector<PCB>RUNNING; // 运行队列，其实每次只会有一个在运行，但这样方便调度
+	void SplitString(const string& s, vector<string>& v, const string& c);//分割字符串的函数
 
 	string IF();  //取指函数,如果取得指令为为NULL,进入缺页中断阻塞，调用内存提供的接口加入阻塞队列
-	void run(int time, int type); //输入指令运行的时间和种类,对于没有明确执行时间的指令，默认为1
-	int ID(string order);  //分析指令
+	void run(int time, char type); //输入指令运行的时间和种类,对于没有明确执行时间的指令，默认为1
+	int ID();  //分析指令
 	int Priority(PCB a, PCB b);  //比较两个进程的优先级，并对其进行调度
 	void ProcessSchedule();  //进程调度，现在的设想是插入排序，在插入的时候直接对优先级进行比较排序
 
-public:
-	void interrupt(int type);  //提供给外部设备以及内存的接口，输入中断的类型
+	void interrupt(PCB process);  //提供给外部设备以及内存的接口，输入中断的类型
 	int ReadApply(string Filename, int time, int size); // 读文件，读取指定文件名的文件
 	int WriteApply(string Filename, int time, int size); // 写文件
 	int MemApply(int size);  //内存申请，传入需要申请的内存的大小
-	int EquipApply(int time, int type); //用于进程申请设备
+	int EquipApply(int time, char type); //用于进程申请设备
 };
 
 
